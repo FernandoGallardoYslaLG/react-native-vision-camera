e94e2322dbe47ace6caffcb58257538a06130b4a
FernandoGallardo <fernando@landgorilla.com>
2024-07-08 00:53:02 -0500

    LGSCRUM-2823 fix: Resolve force portrait photos after take capture

--- package/android/src/main/java/com/mrousavy/camera/core/CameraSession+Photo.kt
+++ package/android/src/main/java/com/mrousavy/camera/core/CameraSession+Photo.kt
@@ -1,11 +1,43 @@
 package com.mrousavy.camera.core
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Matrix
 import android.media.AudioManager
 import android.util.Log
+import android.view.Surface
 import com.mrousavy.camera.core.extensions.takePicture
 import com.mrousavy.camera.core.types.Flash
 import com.mrousavy.camera.core.types.Orientation
 import com.mrousavy.camera.core.utils.FileUtils
+import java.io.File
+import java.io.FileOutputStream
+
+fun rotateImageIfNeeded(photoFile: File, orientation: Int): File {
+  val bitmap = BitmapFactory.decodeFile(photoFile.path)
+  val matrix = Matrix()
+  val rotatedBitmap = when (orientation) {
+    Surface.ROTATION_90 -> {
+      matrix.postRotate(90f)
+      Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
+    }
+    Surface.ROTATION_180 -> {
+      matrix.postRotate(180f)
+      Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
+    }
+    Surface.ROTATION_270 -> {
+      matrix.postRotate(270f)
+      Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
+    }
+    else -> bitmap
+  }
+
+  val rotatedFile = File(photoFile.parent, "rotated_${photoFile.name}")
+  FileOutputStream(rotatedFile).use { out ->
+    rotatedBitmap.compress(Bitmap.CompressFormat.JPEG, 100, out)
+  }
+  return rotatedFile
+}
 
 suspend fun CameraSession.takePhoto(flash: Flash, enableShutterSound: Boolean): Photo {
   val camera = camera ?: throw CameraNotReadyError()
@@ -32,9 +64,10 @@ suspend fun CameraSession.takePhoto(flash: Flash, enableShutterSound: Boolean): 
 
   val size = FileUtils.getImageSize(photoFile.uri.path)
   val rotation = photoOutput.targetRotation
-  val orientation = Orientation.fromSurfaceRotation(rotation)
 
-  return Photo(photoFile.uri.path, size.width, size.height, orientation, isMirrored)
+  val rotatedPhotoFile = rotateImageIfNeeded(File(photoFile.uri.path), rotation)
+
+  return Photo(rotatedPhotoFile.path, size.width, size.height, Orientation.PORTRAIT, isMirrored)
 }
 
 private fun CameraSession.getEnableShutterSoundActual(enable: Boolean): Boolean {
